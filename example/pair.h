8e87cc34996f3f96004807b5d190fb9c9ccc4e84
DevanAnderson <devan.odin.anderson@gmail.com>
2024-02-27 00:30:35 -0800

    finished lab7

---
+++ example/pair.h
@@ -0,0 +1,116 @@
+template <typename FirstType, typename SecondType>
+class Pair {
+public:
+	Pair(FirstType f, SecondType s);
+
+	FirstType getFirst();
+	SecondType getSecond();
+
+private:
+	FirstType first;
+	SecondType second;
+};
+
+template <typename FirstType, typename SecondType>
+Pair<FirstType, SecondType>::Pair(FirstType f, SecondType s)
+		: first(f), second(s) {
+}
+
+template <typename FirstType, typename SecondType>
+FirstType Pair<FirstType, SecondType>::getFirst() {
+	return first;
+}
+
+template <typename FirstType, typename SecondType>
+SecondType Pair<FirstType, SecondType>::getSecond() {
+	return second;	
+}
+ 
+LList::Item::Item(const int& v, Item* p, Item* n)
+: val(v), prev(p), next(n)
+{
+}
+
+LList::LList() {
+  mHead = new Item(int(), nullptr, nullptr);
+  mTail = mHead;
+  mSize = 0;
+}
+
+LList::LList(const LList& other) {
+  mHead = new Item(int(), nullptr, nullptr);
+  mTail = mHead;
+  mSize = 0;
+  Item* temp = other.mHead;
+  while(temp != other.mTail){
+    push_back(temp->val);
+    temp = temp->next;
+  }
+}
+
+LList& LList::operator=(const LList& other) {
+  if(this == &other){
+    return *this;
+  }
+  clear();
+  Item* temp = other.mHead;
+  while(temp != other.mTail){
+    push_back(temp->val);
+    temp = temp->next;
+  }
+  return *this;
+}
+
+LList::~LList() {
+  clear();
+  delete mHead;
+}
+
+int LList::size() const {
+  return mSize;
+}
+
+bool LList::empty() const {
+  return mSize == 0;
+}
+
+void LList::push_back(const int& val) {
+  Item* n = new Item (val, mTail->prev, mTail);
+  if(mHead == mTail){
+    mHead = n;
+  }
+  else
+  {
+    mTail->prev->next = n;
+  }
+  mTail->prev = n;
+  ++mSize;
+}
+
+int& LList::get(int loc) {
+  Item *temp = getNodeAt(loc);
+  return temp->val;
+}
+
+void LList::clear() {
+  while(mHead != mTail) {
+    Item *temp = mHead->next;
+    delete mHead;
+    mHead = temp;
+  }
+  mSize = 0;
+}
+
+typename LList::Item* LList::getNodeAt(int loc) const {
+  if(loc >= 0 && loc < mSize){
+    Item *temp = mHead;
+    while(loc > 0){
+      temp = temp->next;
+      loc--;
+    }
+    return temp;
+  }
+  else {
+    return nullptr;
+  }
+}
